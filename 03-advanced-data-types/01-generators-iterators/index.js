58e94d916c7c8ed66a17af6768d920182ce8388e
Pedro Assis <pedroo16_@hotmail.com>
2023-11-07 19:21:48 -0300

    add advanced data types module

---
+++ 03-advanced-data-types/01-generators-iterators/index.js
@@ -0,0 +1,56 @@
+const assert = require('assert')
+
+function *multiply(value1, value2) {
+  yield value1 * value2
+}
+
+function *main() {
+  yield 'Hello'
+  yield '-'
+  yield 'World'
+  yield *multiply(20, 10)
+}
+
+const generator = main()
+
+assert.deepStrictEqual(generator.next(), { value: 'Hello', done: false })
+assert.deepStrictEqual(generator.next(), { value: '-', done: false })
+assert.deepStrictEqual(generator.next(), { value: 'World', done: false })
+assert.deepStrictEqual(generator.next(), { value: 200, done: false })
+assert.deepStrictEqual(generator.next(), { value: undefined, done: true })
+
+assert.deepStrictEqual(Array.from(main()), [ 'Hello', '-', 'World', 200 ])
+assert.deepStrictEqual([...main()], [ 'Hello', '-', 'World', 200 ])
+
+
+// ---- async iterators
+const { readFile, stat, readdir } = require('fs/promises')
+
+function *promisified() {
+  yield readFile(__filename)
+  yield Promise.resolve('Hey Dude')
+}
+
+Promise.all([...promisified()]).then((results) => console.log('promisified', results))
+// ;(async () => {
+//   for await (const item of promisified()) {
+//     console.log('for await', item.toString())
+//   }
+// })()
+
+async function *systemInfo() {
+  const file = await readFile(__filename)
+  yield { file: file.toString() }
+
+  const { size } = await stat(__filename)
+  yield { size }
+
+  const dir = await readdir(__dirname)
+  yield { dir }
+}
+
+;(async () => {
+  for await (const item of systemInfo()) {
+    console.log('systemInfo', item)
+  }
+})()
---
+++ 03-advanced-data-types/02-symbol/index.js
@@ -0,0 +1,96 @@
+const assert = require('assert')
+
+/* ---- keys */
+const uniqueKey = Symbol('userName')
+const user = {}
+
+user['userName'] = 'value of normal property'
+user[uniqueKey] = 'value of symbol'
+
+assert.deepStrictEqual(user.userName, 'value of normal property')
+// always unique in memory address level
+assert.deepStrictEqual(user[Symbol('userName')], undefined)
+assert.deepStrictEqual(user[uniqueKey], 'value of symbol')
+
+// hard to get but not secret
+assert.deepStrictEqual(Object.getOwnPropertySymbols(user)[0], uniqueKey)
+
+// byPass - bad practice (doesn't even has on node code base)
+user[Symbol.for('password')] = 123
+assert.deepStrictEqual(user[Symbol.for('password')], 123)
+/* ---- keys */
+
+
+/* ---- Well known symbols */
+const obj = {
+  // iterators
+  [Symbol.iterator]: () => ({
+    items: ['c', 'b', 'a'],
+    next() {
+      return {
+        done: this.items.length === 0,
+        value: this.items.pop()
+      }
+    }
+  }),
+}
+
+assert.deepStrictEqual([...obj], ['a', 'b', 'c'])
+
+const kItems = Symbol('kItems')
+class MyDate {
+  constructor(...args) {
+    this[kItems] = args.map((arg) => new Date(...arg))
+  }
+
+  [Symbol.toPrimitive](coercionType) {
+    if (coercionType !== 'string') throw new TypeError()
+
+    const items = this[kItems].map((item) => {
+      return new Intl.DateTimeFormat('pt-BR', { month: 'long', day: '2-digit', year: 'numeric' }).format(item)
+    })
+
+    return new Intl.ListFormat('pt-BR', { style: 'long', type: 'conjunction' }).format(items)
+  }
+
+  *[Symbol.iterator]() {
+    for (const item of this[kItems]) {
+      yield item
+    }
+  }
+
+  async *[Symbol.asyncIterator]() {
+    const timeout = (ms) => new Promise((r) => setTimeout(r, ms))
+    for (const item of this[kItems]) {
+      await timeout(100)
+      yield item.toISOString()
+    }
+  }
+
+  get [Symbol.toStringTag]() {
+    // by default returns 'Object'
+    return 'WHAT?'
+  }
+}
+
+const myDate = new MyDate([2023, 4, 23], [2023, 5, 12])
+const expectedDates = [ new Date(2023, 4, 23), new Date(2023, 5,12)]
+
+assert.deepStrictEqual(Object.prototype.toString.call(myDate), '[object WHAT?]')
+assert.throws(() => myDate + 1, TypeError)
+
+// explicit coercion to call toPrimitive
+assert.deepStrictEqual(String(myDate), '23 de maio de 2023 e 12 de junho de 2023')
+
+assert.deepStrictEqual([...myDate], expectedDates)
+
+// ;(async () => {
+//   for await (const item of myDate) {
+//     console.log('async iterator', item)
+//   }
+// })()
+
+;(async () => {
+  const dates = await Promise.all([...myDate])
+  assert.deepStrictEqual(dates, expectedDates)
+})()
