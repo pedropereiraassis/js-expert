5f2697682ce645cf44e11af6ee455a19c9c81ee8
Pedro Assis <pedroo16_@hotmail.com>
2023-11-06 22:31:37 -0300

    add 05-esmodule project submodule

---
+++ 02-fundamental-concepts/05-esmodules-intl-project/src/index.js
@@ -0,0 +1,33 @@
+import database from '../database.json' assert { type: 'json' }
+import Person from './person.js'
+import { save } from './repository.js'
+import TerminalController from './terminalController.js'
+
+const DEFAULT_LANGUAGE = 'pt-BR'
+const STOP_TERM = ':q'
+
+const terminalController = new TerminalController()
+terminalController.initializeTerminal(database, DEFAULT_LANGUAGE)
+
+async function mainLoop() {
+  try {
+    const answer = await terminalController.question()
+    
+    if (answer === STOP_TERM) {
+      terminalController.closeTerminal()
+      console.log('process finished')
+      return
+    }
+
+    const person = Person.generateInstanceFromString(answer)
+    
+    terminalController.updateTable(person.formatted(DEFAULT_LANGUAGE))
+    await save(person)
+    return mainLoop()
+  } catch (error) {
+    console.error(error)
+    return mainLoop()
+  }
+}
+
+await mainLoop()
