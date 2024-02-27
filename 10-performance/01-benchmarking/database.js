9fe92e070ff110f10e943ae99aa1982efb0385ce
Pedro Assis <pedroo16_@hotmail.com>
2024-02-27 14:43:05 -0300

    add 01 submodule of 10-performance module

---
+++ 10-performance/01-benchmarking/database.js
@@ -0,0 +1,720 @@
+export default {
+  "at": "2024-02-27T16:54:31.998Z",
+  "products": [
+    {
+      "description": "aute tempor officia pariatur",
+      "name": "aute",
+      "price": 743.95,
+      "tmpProperty": undefined,
+      "activePromoId": 8
+    },
+    {},
+    {
+      "description": "ut ipsum duis anim",
+      "name": "nostrud",
+      "price": 1404.52,
+      "tmpProperty": undefined,
+      "activePromoId": 15
+    },
+    {
+      "description": "ex excepteur nisi sunt",
+      "name": "elit",
+      "price": 1768.99,
+      "tmpProperty": undefined,
+      "activePromoId": 67
+    },
+    {},
+    {},
+    {},
+    {
+      "description": "cupidatat occaecat sunt cupidatat",
+      "name": "eu",
+      "price": 940.18,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "officia ad dolore occaecat",
+      "name": "mollit",
+      "price": 1929.09,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "nulla culpa dolore ullamco",
+      "name": "dolore",
+      "price": 586.15,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {},
+    {},
+    {
+      "description": "magna irure commodo voluptate",
+      "name": "excepteur",
+      "price": 1634.88,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {},
+    {},
+    {},
+    {},
+    {},
+    {
+      "description": "Lorem Lorem ullamco ullamco",
+      "name": "magna",
+      "price": 339.26,
+      "tmpProperty": undefined,
+      "activePromoId": 77
+    },
+    {},
+    {
+      "description": "dolore dolor culpa aliqua",
+      "name": "labore",
+      "price": 1036.47,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {},
+    {
+      "description": "magna ipsum labore elit",
+      "name": "Lorem",
+      "price": 842.4,
+      "tmpProperty": undefined,
+      "activePromoId": 85
+    },
+    {
+      "description": "eu et occaecat deserunt",
+      "name": "in",
+      "price": 1611.74,
+      "tmpProperty": undefined,
+      "activePromoId": 44
+    },
+    {},
+    {},
+    {
+      "description": "id consectetur incididunt cillum",
+      "name": "id",
+      "price": 1299.91,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "in duis incididunt sit",
+      "name": "nostrud",
+      "price": 817.88,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "elit qui proident occaecat",
+      "name": "adipisicing",
+      "price": 379.08,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "reprehenderit in enim nulla",
+      "name": "fugiat",
+      "price": 1905.29,
+      "tmpProperty": undefined,
+      "activePromoId": 19
+    },
+    {
+      "description": "Lorem est sit voluptate",
+      "name": "laboris",
+      "price": 892,
+      "tmpProperty": undefined,
+      "activePromoId": 100
+    },
+    {
+      "description": "minim labore ullamco adipisicing",
+      "name": "dolor",
+      "price": 1954.48,
+      "tmpProperty": undefined,
+      "activePromoId": 0
+    },
+    {
+      "description": "do enim cupidatat id",
+      "name": "officia",
+      "price": 1631.77,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "velit voluptate enim laborum",
+      "name": "nulla",
+      "price": 1663.65,
+      "tmpProperty": undefined,
+      "activePromoId": 10
+    },
+    {
+      "description": "veniam labore consequat nostrud",
+      "name": "duis",
+      "price": 1981.5,
+      "tmpProperty": undefined,
+      "activePromoId": 61
+    },
+    {
+      "description": "magna in aute culpa",
+      "name": "incididunt",
+      "price": 989.9,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "qui anim mollit sunt",
+      "name": "fugiat",
+      "price": 547.26,
+      "tmpProperty": undefined,
+      "activePromoId": 80
+    },
+    {
+      "description": "elit mollit deserunt proident",
+      "name": "occaecat",
+      "price": 1707.62,
+      "tmpProperty": undefined,
+      "activePromoId": 28
+    },
+    {
+      "description": "est pariatur in do",
+      "name": "nisi",
+      "price": 497.63,
+      "tmpProperty": undefined,
+      "activePromoId": 77
+    },
+    {
+      "description": "qui dolore ipsum nostrud",
+      "name": "velit",
+      "price": 1652.78,
+      "tmpProperty": undefined,
+      "activePromoId": 69
+    },
+    {
+      "description": "nostrud incididunt anim pariatur",
+      "name": "duis",
+      "price": 409.96,
+      "tmpProperty": undefined,
+      "activePromoId": 11
+    },
+    {
+      "description": "dolor amet non eu",
+      "name": "irure",
+      "price": 362.77,
+      "tmpProperty": undefined,
+      "activePromoId": 46
+    },
+    {
+      "description": "sint laborum reprehenderit et",
+      "name": "occaecat",
+      "price": 450.23,
+      "tmpProperty": undefined,
+      "activePromoId": 99
+    },
+    {
+      "description": "sunt velit exercitation quis",
+      "name": "non",
+      "price": 278.34,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "reprehenderit nostrud dolor culpa",
+      "name": "sit",
+      "price": 310.03,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "mollit laborum proident adipisicing",
+      "name": "reprehenderit",
+      "price": 531.28,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "labore reprehenderit anim ullamco",
+      "name": "amet",
+      "price": 1277.75,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "elit consectetur et ut",
+      "name": "Lorem",
+      "price": 1828.66,
+      "tmpProperty": undefined,
+      "activePromoId": 73
+    },
+    {
+      "description": "proident culpa consequat laboris",
+      "name": "adipisicing",
+      "price": 514.44,
+      "tmpProperty": undefined,
+      "activePromoId": 68
+    },
+    {
+      "description": "ut Lorem adipisicing do",
+      "name": "laboris",
+      "price": 1482.81,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "cillum laboris ea sit",
+      "name": "tempor",
+      "price": 1577.16,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "ex nisi est eiusmod",
+      "name": "consectetur",
+      "price": 1303.91,
+      "tmpProperty": undefined,
+      "activePromoId": 44
+    },
+    {
+      "description": "esse ad consequat pariatur",
+      "name": "pariatur",
+      "price": 317,
+      "tmpProperty": undefined,
+      "activePromoId": 27
+    },
+    {
+      "description": "amet laboris velit do",
+      "name": "aliqua",
+      "price": 1848.28,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "enim minim aliqua aute",
+      "name": "voluptate",
+      "price": 823.23,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "qui aliquip ex culpa",
+      "name": "do",
+      "price": 563.3,
+      "tmpProperty": undefined,
+      "activePromoId": 80
+    },
+    {
+      "description": "consectetur in anim quis",
+      "name": "esse",
+      "price": 326.23,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "ut amet consectetur excepteur",
+      "name": "commodo",
+      "price": 1054.73,
+      "tmpProperty": undefined,
+      "activePromoId": 9
+    },
+    {
+      "description": "fugiat adipisicing veniam nostrud",
+      "name": "est",
+      "price": 477.81,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "reprehenderit qui est aliqua",
+      "name": "ipsum",
+      "price": 1344.2,
+      "tmpProperty": undefined,
+      "activePromoId": 66
+    },
+    {
+      "description": "ex eiusmod laboris fugiat",
+      "name": "proident",
+      "price": 907.67,
+      "tmpProperty": undefined,
+      "activePromoId": 60
+    },
+    {
+      "description": "mollit quis culpa nisi",
+      "name": "fugiat",
+      "price": 376.08,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "eiusmod dolore dolore minim",
+      "name": "do",
+      "price": 1285.5,
+      "tmpProperty": undefined,
+      "activePromoId": 28
+    },
+    {
+      "description": "aliquip irure enim quis",
+      "name": "elit",
+      "price": 1347.27,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "amet eu aliqua eiusmod",
+      "name": "ipsum",
+      "price": 1428.27,
+      "tmpProperty": undefined,
+      "activePromoId": 69
+    },
+    {
+      "description": "incididunt cillum commodo qui",
+      "name": "dolor",
+      "price": 874.36,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "elit pariatur magna anim",
+      "name": "excepteur",
+      "price": 1905.62,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "ea exercitation est deserunt",
+      "name": "sint",
+      "price": 819.32,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "deserunt deserunt culpa officia",
+      "name": "minim",
+      "price": 936.32,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "reprehenderit aliquip sit nulla",
+      "name": "consequat",
+      "price": 769.01,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "id non et laboris",
+      "name": "culpa",
+      "price": 187.03,
+      "tmpProperty": undefined,
+      "activePromoId": 36
+    },
+    {
+      "description": "mollit consectetur duis veniam",
+      "name": "culpa",
+      "price": 357.28,
+      "tmpProperty": undefined,
+      "activePromoId": 54
+    },
+    {
+      "description": "quis occaecat nisi excepteur",
+      "name": "officia",
+      "price": 1421.44,
+      "tmpProperty": undefined,
+      "activePromoId": 30
+    },
+    {
+      "description": "sit deserunt Lorem veniam",
+      "name": "labore",
+      "price": 1374.56,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "id irure nulla adipisicing",
+      "name": "voluptate",
+      "price": 238.84,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "eu labore laboris excepteur",
+      "name": "proident",
+      "price": 716.73,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "magna eiusmod consequat aliqua",
+      "name": "pariatur",
+      "price": 618.31,
+      "tmpProperty": undefined,
+      "activePromoId": 90
+    },
+    {
+      "description": "irure ad consequat id",
+      "name": "dolore",
+      "price": 1371.16,
+      "tmpProperty": undefined,
+      "activePromoId": 67
+    },
+    {
+      "description": "minim ex in commodo",
+      "name": "aute",
+      "price": 936.88,
+      "tmpProperty": undefined,
+      "activePromoId": 66
+    },
+    {
+      "description": "velit cupidatat eu veniam",
+      "name": "non",
+      "price": 989.65,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "elit ullamco aliqua dolore",
+      "name": "laboris",
+      "price": 1458.65,
+      "tmpProperty": undefined,
+      "activePromoId": 43
+    },
+    {
+      "description": "voluptate officia id fugiat",
+      "name": "duis",
+      "price": 1312.68,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "ex ut veniam dolore",
+      "name": "deserunt",
+      "price": 457.79,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "incididunt ullamco proident sit",
+      "name": "fugiat",
+      "price": 1284.21,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "aute Lorem pariatur consectetur",
+      "name": "duis",
+      "price": 1602.85,
+      "tmpProperty": undefined,
+      "activePromoId": 80
+    },
+    {
+      "description": "voluptate in velit mollit",
+      "name": "irure",
+      "price": 907.84,
+      "tmpProperty": undefined,
+      "activePromoId": 41
+    },
+    {
+      "description": "laborum aliqua excepteur laborum",
+      "name": "irure",
+      "price": 704.18,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "irure culpa nulla laborum",
+      "name": "deserunt",
+      "price": 1185.79,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "ea voluptate eu consectetur",
+      "name": "nulla",
+      "price": 1825.89,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "Lorem ipsum et pariatur",
+      "name": "excepteur",
+      "price": 388.73,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "amet reprehenderit do enim",
+      "name": "qui",
+      "price": 198.88,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "dolore ullamco laboris ullamco",
+      "name": "velit",
+      "price": 415.16,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "dolor adipisicing aliquip nostrud",
+      "name": "est",
+      "price": 1579.18,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "ipsum minim consectetur commodo",
+      "name": "reprehenderit",
+      "price": 607.2,
+      "tmpProperty": undefined,
+      "activePromoId": 42
+    },
+    {
+      "description": "sint sint laborum id",
+      "name": "magna",
+      "price": 212.17,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "occaecat ipsum sint voluptate",
+      "name": "anim",
+      "price": 1540.08,
+      "tmpProperty": undefined,
+      "activePromoId": 85
+    },
+    {
+      "description": "qui minim aliquip proident",
+      "name": "laborum",
+      "price": 1925.45,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "aute ea irure eiusmod",
+      "name": "laborum",
+      "price": 1980.73,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "incididunt duis cupidatat dolore",
+      "name": "cillum",
+      "price": 1738.74,
+      "tmpProperty": undefined,
+      "activePromoId": 2
+    },
+    {
+      "description": "mollit dolor dolore qui",
+      "name": "magna",
+      "price": 1794.52,
+      "tmpProperty": undefined,
+      "activePromoId": 79
+    },
+    {
+      "description": "eu magna eiusmod minim",
+      "name": "qui",
+      "price": 721.21,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "in veniam aliquip Lorem",
+      "name": "duis",
+      "price": 495.39,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "ad reprehenderit ut commodo",
+      "name": "esse",
+      "price": 1516.18,
+      "tmpProperty": undefined,
+      "activePromoId": 6
+    },
+    {
+      "description": "reprehenderit Lorem ut quis",
+      "name": "dolor",
+      "price": 772.19,
+      "tmpProperty": undefined,
+      "activePromoId": 48
+    },
+    {
+      "description": "ipsum mollit in officia",
+      "name": "nostrud",
+      "price": 1388.03,
+      "tmpProperty": undefined,
+      "activePromoId": 71
+    },
+    {
+      "description": "sunt reprehenderit pariatur elit",
+      "name": "culpa",
+      "price": 1764.73,
+      "tmpProperty": undefined,
+      "activePromoId": 87
+    },
+    {
+      "description": "aliquip nulla est dolore",
+      "name": "qui",
+      "price": 283.12,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "non ullamco dolore nisi",
+      "name": "voluptate",
+      "price": 1769.35,
+      "tmpProperty": undefined,
+      "activePromoId": 24
+    },
+    {
+      "description": "consequat officia adipisicing duis",
+      "name": "deserunt",
+      "price": 179.66,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "ut anim nisi minim",
+      "name": "eiusmod",
+      "price": 1838.05,
+      "tmpProperty": undefined,
+      "activePromoId": 32
+    },
+    {
+      "description": "quis eu velit aliquip",
+      "name": "et",
+      "price": 1609.24,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "ex deserunt dolor proident",
+      "name": "enim",
+      "price": 489.21,
+      "tmpProperty": undefined,
+      "activePromoId": null
+    },
+    {
+      "description": "ipsum in ipsum ea",
+      "name": "pariatur",
+      "price": 1374.57,
+      "tmpProperty": undefined,
+      "activePromoId": 68
+    },
+    {
+      "description": "nulla fugiat voluptate nisi",
+      "name": "eiusmod",
+      "price": 1893.29,
+      "tmpProperty": undefined,
+      "activePromoId": 73
+    },
+    {
+      "description": "dolor tempor ipsum nostrud",
+      "name": "enim",
+      "price": 236.01,
+      "tmpProperty": undefined,
+      "activePromoId": 67
+    }
+  ]
+}
