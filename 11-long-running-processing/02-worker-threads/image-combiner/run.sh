238948e6715f43e3a5fc0c39b647d56731a85dc9
Pedro Assis <pedroo16_@hotmail.com>
2024-03-19 10:53:09 -0300

    add 02-worker-threads submodule on 11-long-running-processing module

---
+++ 11-long-running-processing/02-worker-threads/image-combiner/run.sh
@@ -0,0 +1,6 @@
+IMAGE_URL="https://static.wikia.nocookie.net/mkwikia/images/e/ee/Predator_render.png"
+BACKGROUND_URL="https://wallpaperaccess.com/full/3057585.jpg"
+
+curl "http://localhost:3000/joinImages?img=$IMAGE_URL&background=$BACKGROUND_URL"
+# use 'piscina' lib to create the connection pool and run autocannon
+# npx autocannon --renderStatusCodes -c 500 "http://localhost:3000/joinImages?img=$IMAGE_URL&background=$BACKGROUND_URL"
